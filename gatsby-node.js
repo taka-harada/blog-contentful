44d4815192be52ddd88b0dc82763d79572dd2040
Taka Harada <harada@hivelocity.co.jp>
2020-04-15 16:59:07 +0900

    Initial Commit

---
+++ gatsby-node.js
@@ -0,0 +1,64 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+
+const path = require(`path`);
+const slash = require(`slash`);
+
+// 動的にページを生成する関数
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  return graphql(
+    `
+    {
+      allContentfulBlog {
+        edges {
+          node {
+            id
+            slug
+            title
+            featureImage {
+              title
+              resize {
+                src
+              }
+            }
+            publishDate
+            body {
+              body
+              childMarkdownRemark {
+                html
+              }
+            }
+            tags
+          }
+        }
+      }
+    }
+    `
+  ).then(result => {
+    if (result.errors) {
+      console.log("Error retrieving contentful data", result.errors);
+    }
+
+    // 利用するテンプレートファイルを指定
+    const blogPostTemplate = path.resolve("./src/templates/blogpost.js");
+    result.data.allContentfulBlog.edges.forEach(edge => {
+      createPage({
+        path: `/blogpost/${edge.node.slug}`,
+        component: slash(blogPostTemplate),
+        context: {
+          slug: edge.node.slug,
+          id: edge.node.id
+        }
+      });
+    });
+  })
+  .catch(error => {
+    console.log("Error retrieving contentful data", error);
+  })
+}
---
+++ src/templates/blogpost.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+const BlogPost = ({ data }) => {
+  const { title, featureImage, publishDate, body , tags } = data.contentfulBlog;
+  const contentHtml = data.contentfulBlog.body.childMarkdownRemark.html
+  return (
+    <Layout>
+      <SEO title={title} />
+      <div className="blogpost">
+        <h1>{title}</h1>
+        <p>{publishDate}</p>
+        <img src={featureImage.resize.src} alt={featureImage.title}/>
+        <div className="tags">
+          {tags.map(tag => (
+            <li className="tag" key={tag}>
+              {tag}
+            </li>
+          ))}
+        </div>        
+        <div dangerouslySetInnerHTML={{ __html: contentHtml }}></div>
+        <Link to="/">Back to Home</Link>
+      </div>
+    </Layout>
+  );
+};
+export default BlogPost;
+export const pageQuery = graphql`
+  query($slug: String!) {
+    contentfulBlog(slug: { eq: $slug }) {
+      title
+      featureImage {
+        title
+        resize {
+          src
+        }
+      }
+      publishDate(formatString: "Do MMMM, YYYY")
+      slug
+      body {
+        body
+        childMarkdownRemark {
+          html
+        }
+      }
+      tags
+    }
+  }
+`;
