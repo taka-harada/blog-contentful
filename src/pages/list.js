33e06f773451fcfd34669acd1d1c04ed80a2c8e9
Taka Harada <harada@hivelocity.co.jp>
2020-05-08 18:56:37 +0900

    Fix name of post archive page

--- src/pages/list.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-import ListTemplate from "../templates/list-temp"
-
-export default ({ }) => {
-  const siteTitle = "demo";
-  return <ListTemplate siteTitle={siteTitle} />;
-}
