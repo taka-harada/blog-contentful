d6806dc3a32541a93efd06a4063182c89d4a1942
Taka Harada <harada@hivelocity.co.jp>
2020-05-20 19:18:29 +0900

    Fix difinition for using localstorage

--- src/components/molecules/HeadSearch.js
+++ src/components/molecules/HeadSearch.js
@@ -1,12 +1,17 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import styled from "styled-components"
 import SearchIcon from "../../images/button_search-new.svg"
 import SearchField from "../atoms/SearchField"
 
-let text = localStorage.getItem("text") || "";
+//let text = localStorage.getItem("text") || "";
 
 const HeadSearch = () => {
+  const [text, setText] = useState('')
   const [flag, setFlag] = useState(true);
+
+  useEffect(() => {
+    setText(localStorage.getItem("text"))
+  })
 
   // const openModal = () => {
   //   setFlag(!flag)
