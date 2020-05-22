441d879ce814d1a9f4d835892a145baafc642f30
Taka Harada <harada@hivelocity.co.jp>
2020-05-22 19:29:09 +0900

    Install node-sass & gatsby-sass-plugin for using SCSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,7 +49,8 @@ module.exports = {
         ],
         display: 'swap'
       }
-    }
+    },
+    `gatsby-plugin-sass`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
